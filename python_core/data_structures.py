b59b3382fd612fda9522b15a86f1e0c592fd456b
Linda Yamil Garaycochea Saune <lindaysaune21@gmail.com>
2025-11-30 07:20:15 -0500

    Data structures as ML-ready inputs

---
+++ python_core/data_structures.py
@@ -0,0 +1,13 @@
+# In ML, lists often represent datasets
+logs = [
+    "login success",
+    "login failed",
+    "unknown device"
+]
+
+# Dictionaries often represent features
+log_features = {
+    "event_type": "login",
+    "status": "failed",
+    "risk_score": 7
+}
